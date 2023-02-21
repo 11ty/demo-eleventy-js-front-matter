0a7270624c414bae7d4c16ceb0b57d49d830bba2
Zach Leatherman <zachleatherman@gmail.com>
2023-02-21 13:36:53 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# demo-eleventy-javascript-front-matter
+
+A demo showing how to use free-form JavaScript in front matter to populate the data cascade.
+
+Check out `page.njk` for a Nunjucks example but this will work in any Eleventy template type (including Custom ones).
---
+++ eleventy.config.js
@@ -0,0 +1,21 @@
+const { RetrieveGlobals } = require("node-retrieve-globals");
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addFilter("await", async promise => {
+		return promise;
+	});
+
+	eleventyConfig.setFrontMatterParsingOptions({
+		engines: {
+			"javascript": function(frontMatterCode) {
+				let vm = new RetrieveGlobals(frontMatterCode);
+
+				let data = {}; // want to pass in data available in front matter?
+				return vm.getGlobalContext(data, {
+					reuseGlobal: true,
+					dynamicImport: true,
+				});
+			}
+		}
+	});
+};
